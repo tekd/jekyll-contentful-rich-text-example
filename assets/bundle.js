8f8bc3acf5ce8cbc36d117a51bdd24c1443631b2
tekd <tekd@users.noreply.github.com>
2025-09-09 00:44:07 +0000

    deploy: b38b6ac58802c2ba0dae3726daa3a6ca33d45db5

--- assets/bundle.js
+++ assets/bundle.js
@@ -1,1 +1,158 @@
-(()=>{var e={953:e=>{var t={openModal:function(){$(".js-open-modal").click((function(e){$(e.currentTarget).parent().find(".js-target-modal").addClass("js-active"),$("#overlay").addClass("js-active"),$("body").addClass("js-body-modal-active")}))},closeModal:function(){$("body").on("click",".js-close-modal",(function(){$(".js-target-modal").removeClass("js-active"),$("#overlay").removeClass("js-active"),$("body").removeClass("js-body-modal-active")}))},clickOverlayCloseModal:function(){$("#overlay").click((function(){$("body").removeClass("js-body-modal-active")}))},init:function(){this.openModal(),this.closeModal(),this.clickOverlayCloseModal()}};e.exports=t},257:e=>{var t={mainMenu:function(){$(".js-trigger-menu").click((function(e){$(e.currentTarget).next().addClass("js-active-menu"),$("#overlay").addClass("js-active")}))},init:function(){this.mainMenu()}};e.exports=t},34:e=>{var t={onClickOverlay:function(){$("#overlay").click((function(){$(".js-active").removeClass("js-active"),$(".js-active-menu").removeClass("js-active-menu")}))},init:function(){this.onClickOverlay()}};e.exports=t},596:e=>{var t={smoothScrolling:function(){$("a[href*=\\#]:not([href=\\#])").on("click",(function(e){var t=$(e.currentTarget.hash),n=e.currentTarget.hostname,a=window.location.hostname,o=e.currentTarget.pathname.replace(/^\//,""),r=window.location.pathname.replace(/^\//,"");return t.length||(t=$("[name=".concat(e.currentTarget.hash.slice(1),"]"))),!t.length||n!==a&&o!==r||($("html, body").animate({scrollTop:t.offset().top},1e3),!1)}))},init:function(){this.smoothScrolling()}};e.exports=t},954:e=>{var t={fixedSearchTrigger:function(){$(".js-trigger-search").on("click",(function(e){e.preventDefault(),$(e.currentTarget).parent().addClass("js-active"),$("#overlay").addClass("js-active")}))},init:function(){this.fixedSearchTrigger()}};e.exports=t},232:e=>{var t={markdownLinksNewPage:function(){$(".markdown a").map((function(e,t){return t.setAttribute("target","_blank")}))}};e.exports=t}},t={};function n(a){var o=t[a];if(void 0!==o)return o.exports;var r=t[a]={exports:{}};return e[a](r,r.exports,n),r.exports}n.n=e=>{var t=e&&e.__esModule?()=>e.default:()=>e;return n.d(t,{a:t}),t},n.d=(e,t)=>{for(var a in t)n.o(t,a)&&!n.o(e,a)&&Object.defineProperty(e,a,{enumerable:!0,get:t[a]})},n.o=(e,t)=>Object.prototype.hasOwnProperty.call(e,t),(()=>{"use strict";var e=n(34),t=n.n(e),a=n(953),o=n.n(a),r=n(257),i=n.n(r),s=n(596),c=n.n(s),l=n(954),d=n.n(l),u=n(232),v=n.n(u);o().init(),i().init(),t().init(),c().init(),d().init(),v().markdownLinksNewPage()})()})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./source/_js/modal.js":
+/*!*****************************!*\
+  !*** ./source/_js/modal.js ***!
+  \*****************************/
+/***/ ((module) => {
+
+eval("var Modal = {\n  openModal: function openModal() {\n    $('.js-open-modal').click(function (e) {\n      $(e.currentTarget).parent().find('.js-target-modal').addClass('js-active');\n      $('#overlay').addClass('js-active');\n      $('body').addClass('js-body-modal-active');\n    });\n  },\n  closeModal: function closeModal() {\n    $('body').on('click', '.js-close-modal', function () {\n      $('.js-target-modal').removeClass('js-active');\n      $('#overlay').removeClass('js-active');\n      $('body').removeClass('js-body-modal-active');\n    });\n  },\n  clickOverlayCloseModal: function clickOverlayCloseModal() {\n    $('#overlay').click(function () {\n      $('body').removeClass('js-body-modal-active');\n    });\n  },\n  init: function init() {\n    this.openModal();\n    this.closeModal();\n    this.clickOverlayCloseModal();\n  }\n};\nmodule.exports = Modal;\n\n//# sourceURL=webpack://objectively-boilerplate/./source/_js/modal.js?");
+
+/***/ }),
+
+/***/ "./source/_js/nav.js":
+/*!***************************!*\
+  !*** ./source/_js/nav.js ***!
+  \***************************/
+/***/ ((module) => {
+
+eval("var Nav = {\n  mainMenu: function mainMenu() {\n    // Main Menu Click Behavior\n    $('.js-trigger-menu').click(function (e) {\n      $(e.currentTarget).next().addClass('js-active-menu');\n      $('#overlay').addClass('js-active');\n    });\n  },\n  init: function init() {\n    this.mainMenu();\n  }\n};\nmodule.exports = Nav;\n\n//# sourceURL=webpack://objectively-boilerplate/./source/_js/nav.js?");
+
+/***/ }),
+
+/***/ "./source/_js/overlay.js":
+/*!*******************************!*\
+  !*** ./source/_js/overlay.js ***!
+  \*******************************/
+/***/ ((module) => {
+
+eval("var Overlay = {\n  onClickOverlay: function onClickOverlay() {\n    $('#overlay').click(function () {\n      $('.js-active').removeClass('js-active');\n      $('.js-active-menu').removeClass('js-active-menu');\n    });\n  },\n  init: function init() {\n    this.onClickOverlay();\n  }\n};\nmodule.exports = Overlay;\n\n//# sourceURL=webpack://objectively-boilerplate/./source/_js/overlay.js?");
+
+/***/ }),
+
+/***/ "./source/_js/scripts.js":
+/*!*******************************!*\
+  !*** ./source/_js/scripts.js ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _overlay__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./overlay */ \"./source/_js/overlay.js\");\n/* harmony import */ var _overlay__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_overlay__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _modal__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modal */ \"./source/_js/modal.js\");\n/* harmony import */ var _modal__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_modal__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _nav__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./nav */ \"./source/_js/nav.js\");\n/* harmony import */ var _nav__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_nav__WEBPACK_IMPORTED_MODULE_2__);\n/* harmony import */ var _scroll__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./scroll */ \"./source/_js/scroll.js\");\n/* harmony import */ var _scroll__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_scroll__WEBPACK_IMPORTED_MODULE_3__);\n/* harmony import */ var _search__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./search */ \"./source/_js/search.js\");\n/* harmony import */ var _search__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_search__WEBPACK_IMPORTED_MODULE_4__);\n/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./utils */ \"./source/_js/utils.js\");\n/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_utils__WEBPACK_IMPORTED_MODULE_5__);\n\n\n\n\n\n\n_modal__WEBPACK_IMPORTED_MODULE_1___default().init();\n_nav__WEBPACK_IMPORTED_MODULE_2___default().init();\n_overlay__WEBPACK_IMPORTED_MODULE_0___default().init();\n_scroll__WEBPACK_IMPORTED_MODULE_3___default().init();\n_search__WEBPACK_IMPORTED_MODULE_4___default().init();\n_utils__WEBPACK_IMPORTED_MODULE_5___default().markdownLinksNewPage();\n\n//# sourceURL=webpack://objectively-boilerplate/./source/_js/scripts.js?");
+
+/***/ }),
+
+/***/ "./source/_js/scroll.js":
+/*!******************************!*\
+  !*** ./source/_js/scroll.js ***!
+  \******************************/
+/***/ ((module) => {
+
+eval("var Scroll = {\n  smoothScrolling: function smoothScrolling() {\n    // Smooth Scrolling Function\n    $('a[href*=\\\\#]:not([href=\\\\#])').on('click', function (e) {\n      var $targ = $(e.currentTarget.hash);\n      var host1 = e.currentTarget.hostname;\n      var host2 = window.location.hostname;\n      var path1 = e.currentTarget.pathname.replace(/^\\//, '');\n      var path2 = window.location.pathname.replace(/^\\//, '');\n      if (!$targ.length) {\n        $targ = $(\"[name=\".concat(e.currentTarget.hash.slice(1), \"]\"));\n      }\n      if ($targ.length && (host1 === host2 || path1 === path2)) {\n        $('html, body').animate({\n          scrollTop: $targ.offset().top\n        }, 1000);\n        return false;\n      }\n      return true;\n    });\n  },\n  init: function init() {\n    this.smoothScrolling();\n  }\n};\nmodule.exports = Scroll;\n\n//# sourceURL=webpack://objectively-boilerplate/./source/_js/scroll.js?");
+
+/***/ }),
+
+/***/ "./source/_js/search.js":
+/*!******************************!*\
+  !*** ./source/_js/search.js ***!
+  \******************************/
+/***/ ((module) => {
+
+eval("var Search = {\n  fixedSearchTrigger: function fixedSearchTrigger() {\n    // Search Click Behavior\n    $('.js-trigger-search').on('click', function (e) {\n      e.preventDefault();\n      $(e.currentTarget).parent().addClass('js-active');\n      $('#overlay').addClass('js-active');\n    });\n  },\n  init: function init() {\n    this.fixedSearchTrigger();\n  }\n};\nmodule.exports = Search;\n\n//# sourceURL=webpack://objectively-boilerplate/./source/_js/search.js?");
+
+/***/ }),
+
+/***/ "./source/_js/utils.js":
+/*!*****************************!*\
+  !*** ./source/_js/utils.js ***!
+  \*****************************/
+/***/ ((module) => {
+
+eval("var Utils = {\n  markdownLinksNewPage: function markdownLinksNewPage() {\n    $('.markdown a').map(function (idx, link) {\n      return link.setAttribute('target', '_blank');\n    });\n  }\n};\nmodule.exports = Utils;\n\n//# sourceURL=webpack://objectively-boilerplate/./source/_js/utils.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./source/_js/scripts.js");
+/******/ 	
+/******/ })()
+;
